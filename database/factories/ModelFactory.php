e397217063e90592aa7a6e0a1646708492af6b6f
Stephanie <stephanieledig@hotmail.com>
2016-07-23 13:16:49 +0200

    modelfactory ready

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -14,8 +14,21 @@
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
+        'username' =>$faker->userName,
         'email' => $faker->safeEmail,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Apero::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->text,
+        'content' => $faker->text,
+        'abstract' => $faker->text,
+        'status' => $faker->randomElement(array('published', 'unpublished')),
+        'published_on' => $faker->dateTime->format('d,M,Y'),
+        'date' => $faker->date($format = 'd-m-Y', $max = 'now'),
+    ];
+});
+
